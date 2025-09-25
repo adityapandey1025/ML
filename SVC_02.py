564e21c9933ab366565bf9a4d7f513eda3c9ead6
adityapandey1025 <adityakumarpandey1025@gmail.com>
2025-09-26 00:13:38 +0530

    update

--- SVC_02.py
+++ SVC_02.py
@@ -1,9 +1,8 @@
 import numpy as np
 import pandas as pd
-from sklearn.discriminant_analysis import StandardScaler
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 
 # Load dataset
@@ -21,7 +20,7 @@ X_label=df.drop(["category","id","name"],axis=1)
 y_label=df["category"]
 
 scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+X_scaled = scaler.fit_transform(X_label)
 
 # Train-test split
 train_x,test_x,train_y,test_y=train_test_split(X_scaled,y_label,test_size=0.2,random_state=42)
